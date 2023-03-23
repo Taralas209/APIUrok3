123ab90e3e17b2e9635bea28d78d2377b36bd449
Taralas209 <alexej.zikunow@gmail.com>
2023-03-23 11:28:57 +0100

    Added two spaces between functions

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
+
 def shorten_link(link, access_token, custom_title):
     bitly_linkshortener_url = 'https://api-ssl.bitly.com/v4/shorten'
     headers = {
@@ -16,6 +17,7 @@ def shorten_link(link, access_token, custom_title):
     shortened_link_response = requests.post(bitly_linkshortener_url, headers=headers, json=payload)
     shortened_link_response.raise_for_status()
     return shortened_link_response.json()['link']
+
 
 def count_link_clicks(link, access_token):
     parsed_link = urlparse(link)
@@ -30,6 +32,7 @@ def count_link_clicks(link, access_token):
     clicks_summary_response = requests.get(bitly_linkcounter_url, headers=headers, params=payload)
     clicks_summary_response.raise_for_status()
     return clicks_summary_response.json()['total_clicks']
+
 
 def is_bitlink(link, access_token):
     parsed_link = urlparse(link)
@@ -49,6 +52,7 @@ def get_link():
     link = args.url_link
     return link
 
+
 def main():
     load_dotenv()
     access_token = os.environ.get('BITLY_ACCESS_TOKEN')
@@ -64,6 +68,8 @@ def main():
             print(shortened_link)
     except requests.exceptions.HTTPError as error:
          print(f"HTTP error occurred: {error}")
+
+
 
 if __name__ =="__main__":
     main()
